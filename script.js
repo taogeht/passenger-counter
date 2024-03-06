9fdefcad0b05b5f6bb6109dbcce0cb2ebb913d91
Bryce Vancuren <brycev@gmail.com>
2024-03-07 06:38:03 +0800

    Commit initial files
    I did the first thing

---
+++ script.js
@@ -0,0 +1,15 @@
+let saveEl = document.getElementById("save-el")
+let countEL = document.getElementById("count-el")
+let count = 0
+
+function increment(){
+    count += 1
+    countEL.textContent = count
+}
+
+function save(){
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    countEL.textContent = 0
+    count = 0
+}
